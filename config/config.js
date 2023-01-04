26451a2dbf5b58c9da3958a6ed10d5c296e67cc0
unknown <doyinadeyemi2000@gmail.com>
2023-01-04 05:29:12 +0100

    fixed database config

--- config/config.js
+++ config/config.js
@@ -1,27 +1,30 @@
-require("dotenv").config();
+require("dotenv").config()
 
 module.exports = {
     development: {
-		username: process.env.DBUSERNAME || "root",
-		password: process.env.DBPASS || null,
-		database: "voting_platform",
-		host: "127.0.0.1",
-		dialect: "mysql",
-	},
-	test: {
+        username: process.env.db_username || "root",
+        password: process.env.db_password || null,
+        database: process.env.db_database || "voting_platform",
+        host: process.env.db_host || "127.0.0.1",
+        dialect: "mysql",
+        port: process.env.db_port,
+    },
+    test: {
         logging: false,
-		username: process.env.DBUSERNAME || "root",
-		password: process.env.DBPASS || null,
-		database: "database_test",
-		host: "127.0.0.1",
-		dialect: "mysql",
-	},
-	production: {
+        username: process.env.db_username || "root",
+        password: process.env.db_password || null,
+        database: process.env.db_database || "database_production",
+        host: process.env.db_host || "127.0.0.1",
+        dialect: "mysql",
+        port: process.env.db_port,
+    },
+    production: {
         logging: false,
-		username: process.env.DBUSERNAME || "root",
-		password: process.env.DBPASS || null,
-		database: "database_production",
-		host: "127.0.0.1",
-		dialect: "mysql",
-	},
-};
+        username: process.env.db_username || "root",
+        password: process.env.db_password || null,
+        database: process.env.db_database || "database_production",
+        host: process.env.db_host || "127.0.0.1",
+        dialect: "mysql",
+        port: process.env.db_port,
+    },
+}
