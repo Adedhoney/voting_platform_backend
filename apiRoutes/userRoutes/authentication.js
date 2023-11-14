acd81ed6da726acc62f8495cc70ac74bd9650d4c
Sherrifdeen Tijani <43505184+tsherrifdeen@users.noreply.github.com>
2023-11-14 16:38:58 +0100

    changes

--- apiRoutes/userRoutes/authentication.js
+++ apiRoutes/userRoutes/authentication.js
@@ -1,29 +1,29 @@
-const db = require("../../models")
-const bcrypt = require("bcrypt")
-const dotenv = require("dotenv")
-const jwt = require("jsonwebtoken")
+const db = require("../../models");
+const bcrypt = require("bcrypt");
+const dotenv = require("dotenv");
+const jwt = require("jsonwebtoken");
 
 module.exports.logIn = async (req, res) => {
-    try {
-        const loginDetails = await db.user.findByPk(req.query.matricNO)
-        if (!loginDetails) {
-            return res.status(404).json({ message: "User not found" })
-        }
-        const correctPassword = await bcrypt.compare(
-            req.query.password,
-            loginDetails.password
-        )
-        if (!correctPassword) {
-            return res.status(400).json({ message: "Incorrect Password" })
-        }
-
-        dotenv.config()
-        const jwToken = jwt.sign(
-            loginDetails.user_id,
-            process.env.User_Access_Token
-        )
-        res.json({ accessToken: jwToken })
-    } catch (error) {
-        res.status(500).json({ message: "Error while logging in" })
+  try {
+    const loginDetails = await db.user.findByPk(req.query.matricNO);
+    if (!loginDetails) {
+      return res.status(404).json({ message: "User not found" });
     }
-}
+    const correctPassword = await bcrypt.compare(
+      req.query.password,
+      loginDetails.password
+    );
+    if (!correctPassword) {
+      return res.status(400).json({ message: "Incorrect Password" });
+    }
+
+    dotenv.config();
+    const jwToken = jwt.sign(
+      loginDetails.user_id,
+      process.env.User_Access_Token
+    );
+    res.json({ accessToken: jwToken });
+  } catch (error) {
+    res.status(500).json({ message: "Error while logging in" });
+  }
+};
--- config/config.js
+++ config/config.js
@@ -1,30 +1,30 @@
-require("dotenv").config()
+require("dotenv").config();
 
 module.exports = {
-    development: {
-        username: process.env.db_username || "root",
-        password: process.env.db_password || null,
-        database: process.env.db_database || "voting_platform",
-        host: process.env.db_host || "127.0.0.1",
-        dialect: "mysql",
-        port: process.env.db_port,
-    },
-    test: {
-        logging: false,
-        username: process.env.db_username || "root",
-        password: process.env.db_password || null,
-        database: process.env.db_database || "database_production",
-        host: process.env.db_host || "127.0.0.1",
-        dialect: "mysql",
-        port: process.env.db_port,
-    },
-    production: {
-        logging: false,
-        username: process.env.db_username || "root",
-        password: process.env.db_password || null,
-        database: process.env.db_database || "database_production",
-        host: process.env.db_host || "127.0.0.1",
-        dialect: "mysql",
-        port: process.env.db_port,
-    },
-}
+  development: {
+    username: process.env.db_username || "root",
+    password: process.env.db_password || null,
+    database: process.env.db_database || "voting_platform",
+    host: process.env.db_host || "127.0.0.1",
+    dialect: "mysql",
+    port: process.env.db_port,
+  },
+  test: {
+    logging: false,
+    username: process.env.db_username || "root",
+    password: process.env.db_password || null,
+    database: process.env.db_database || "database_production",
+    host: process.env.db_host || "127.0.0.1",
+    dialect: "mysql",
+    port: process.env.db_port,
+  },
+  production: {
+    logging: false,
+    username: process.env.db_username || "root",
+    password: process.env.db_password || null,
+    database: process.env.db_database || "database_production",
+    host: process.env.db_host || "127.0.0.1",
+    dialect: "mysql",
+    port: process.env.db_port,
+  },
+};
