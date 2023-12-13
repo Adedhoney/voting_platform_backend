9b9c16518f0c3899bb07f3cfc6fa7c3c7cd50e97
Sherrifdeen Tijani <43505184+tsherrifdeen@users.noreply.github.com>
2023-12-13 11:50:49 +0100

    updates

--- apiRoutes/adminRoutes/overview.js
+++ apiRoutes/adminRoutes/overview.js
@@ -1,84 +1,84 @@
+// const db = require("../../models");
+
+// const getOverview = async (req, res) => {
+//   try {
+//     // GET POSITIONS
+//     let positions = await db.position.findAll({
+//       order: [["createdAt", "ASC"]], // Order positions by creation time in ascending order
+//     });
+
+//     for (let position of positions) {
+//       let voteCount = await db.vote.count({
+//         where: { position_id: position.position_id },
+//       });
+//       position.dataValues.votes = voteCount;
+//     }
+
+//     // GET CANDIDATES
+//     let candidates = await db.candidate.findAll({
+//       order: [["createdAt", "ASC"]], // Order candidates by creation time in ascending order
+//     });
+
+//     for (let candidate of candidates) {
+//       let voteCount = await db.vote.count({
+//         where: { candidate_id: candidate.candidate_id },
+//       });
+//       candidate.dataValues.votes = voteCount;
+//     }
+
+//     // GET NUMBER OF USERS THAT HAVE VOTED
+//     let usersVoted = await db.user.count({
+//       where: { vote_status: true },
+//     });
+
+//     // GET TOTAL NUMBER OF USERS
+//     let totalUsers = await db.user.count();
+
+//     return res.json({ positions, candidates, usersVoted, totalUsers });
+//   } catch (error) {
+//     console.error(error);
+//     return res
+//       .status(501)
+//       .json({ message: "Error occurred when getting results" });
+//   }
+// };
+
+// module.exports.getOverview = getOverview;
+
 const db = require("../../models");
 
 const getOverview = async (req, res) => {
   try {
-    // GET POSITIONS
-    let positions = await db.position.findAll({
-      order: [["createdAt", "ASC"]], // Order positions by creation time in ascending order
-    });
-
+    // GET POSITIIONS
+    let positions = await db.position.findAll({});
     for (let position of positions) {
       let voteCount = await db.vote.count({
         where: { position_id: position.position_id },
       });
       position.dataValues.votes = voteCount;
     }
-
-    // GET CANDIDATES
-    let candidates = await db.candidate.findAll({
-      order: [["createdAt", "ASC"]], // Order candidates by creation time in ascending order
-    });
-
+    // get candidates
+    let candidates = await db.candidate.findAll({});
     for (let candidate of candidates) {
       let voteCount = await db.vote.count({
         where: { candidate_id: candidate.candidate_id },
       });
       candidate.dataValues.votes = voteCount;
     }
-
-    // GET NUMBER OF USERS THAT HAVE VOTED
+    // get number of uses that have voted
     let usersVoted = await db.user.count({
       where: { vote_status: true },
     });
-
-    // GET TOTAL NUMBER OF USERS
+    // get total number of users
     let totalUsers = await db.user.count();
 
     return res.json({ positions, candidates, usersVoted, totalUsers });
   } catch (error) {
-    console.error(error);
+    console.log(error);
     return res
       .status(501)
-      .json({ message: "Error occurred when getting results" });
+      .json({ message: "Error occured when getting results" });
   }
 };
 
 module.exports.getOverview = getOverview;
-
-// const db = require("../../models")
-
-// const getOverview = async (req, res) => {
-//     try {
-//         // GET POSITIIONS
-//         let positions = await db.position.findAll({})
-//         for (let position of positions) {
-//             let voteCount = await db.vote.count({
-//                 where: { position_id: position.position_id },
-//             })
-//             position.dataValues.votes = voteCount
-//         }
-//         // get candidates
-//         let candidates = await db.candidate.findAll({})
-//         for (let candidate of candidates) {
-//             let voteCount = await db.vote.count({
-//                 where: { candidate_id: candidate.candidate_id },
-//             })
-//             candidate.dataValues.votes = voteCount
-//         }
-//         // get number of uses that have voted
-//         let usersVoted = await db.user.count({
-//             where: { vote_status: true },
-//         })
-//         // get total number of users
-//         let totalUsers = await db.user.count()
-
-//         return res.json({ positions, candidates, usersVoted, totalUsers })
-//     } catch (error) {
-//         console.log(error)
-//         return res
-//             .status(501)
-//             .json({ message: "Error occured when getting results" })
-//     }
-// }
-
-// module.exports.getOverview = getOverview
--- apiRoutes/userRoutes/getData.js
+++ apiRoutes/userRoutes/getData.js
@@ -4,33 +4,33 @@ module.exports.getUserData = async (req, res) => {
   const userData = await db.user.findByPk(req.userId);
   res.json(userData);
 };
-// module.exports.getElectionData = async (req, res) => {
-//     const positions = await db.position.findAll()
-//     const candidateInfo = await db.candidate.findAll({
-//         include: db.position,
-//     })
-//     res.json({
-//         positions: [...positions],
-//         candidates: [...candidateInfo],
-//     })
-// }
 module.exports.getElectionData = async (req, res) => {
-  try {
-    const positions = await db.position.findAll({
-      order: [["createdAt", "ASC"]], // Order by creation time in ascending order
-    });
-
-    const candidateInfo = await db.candidate.findAll({
-      include: db.position,
-      order: [["createdAt", "ASC"]], // Order by creation time in ascending order
-    });
-
-    res.json({
-      positions: [...positions],
-      candidates: [...candidateInfo],
-    });
-  } catch (error) {
-    console.error("Error fetching election data:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
+  const positions = await db.position.findAll();
+  const candidateInfo = await db.candidate.findAll({
+    include: db.position,
+  });
+  res.json({
+    positions: [...positions],
+    candidates: [...candidateInfo],
+  });
 };
+// module.exports.getElectionData = async (req, res) => {
+//   try {
+//     const positions = await db.position.findAll({
+//       order: [["createdAt", "ASC"]], // Order by creation time in ascending order
+//     });
+
+//     const candidateInfo = await db.candidate.findAll({
+//       include: db.position,
+//       order: [["createdAt", "ASC"]], // Order by creation time in ascending order
+//     });
+
+//     res.json({
+//       positions: [...positions],
+//       candidates: [...candidateInfo],
+//     });
+//   } catch (error) {
+//     console.error("Error fetching election data:", error);
+//     res.status(500).json({ error: "Internal Server Error" });
+//   }
+// };
